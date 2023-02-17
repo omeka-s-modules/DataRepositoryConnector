5b5489d0b00c032a807bb6f8075558c9f412e2d5
matthewjmckinley <matthewjamesmckinley@gmail.com>
2023-02-17 13:45:58 -0800

    Link to advanced search results for items imported by jobs on Past Imports page

--- Module.php
+++ Module.php
@@ -63,10 +63,16 @@ class Module extends AbstractModule
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
         $sharedEventManager->attach(
-                'Omeka\Controller\Admin\Item',
-                'view.show.after',
-                [$this, 'showSource']
-                );
+            'Omeka\Controller\Admin\Item',
+            'view.show.after',
+            [$this, 'showSource']
+        );
+
+        $sharedEventManager->attach(
+            \Omeka\Api\Adapter\ItemAdapter::class,
+            'api.search.query',
+            [$this, 'importSearch']
+        );
     }
 
     public function showSource($event)
@@ -81,6 +87,23 @@ class Module extends AbstractModule
             echo '<h3>' . $view->translate('Original') . '</h3>';
             echo '<p>' . $view->translate('Last Modified') . ' ' . $view->i18n()->dateFormat($dataItem->lastModified()) . '</p>';
             echo '<p><a href="' . $dataItem->uri() . '">' . $view->translate('Link') . '</a></p>';
+        }
+    }
+
+    public function importSearch($event)
+    {
+        $query = $event->getParam('request')->getContent();
+        if (isset($query['data_import_id'])) {
+            $qb = $event->getParam('queryBuilder');
+            $adapter = $event->getTarget();
+            $importItemAlias = $adapter->createAlias();
+            $qb->innerJoin(
+                \DataRepositoryConnector\Entity\DataRepositoryItem::class, $importItemAlias,
+                'WITH', "$importItemAlias.item = omeka_root.id"
+            )->andWhere($qb->expr()->eq(
+                "$importItemAlias.job",
+                $adapter->createNamedParameter($qb, $query['data_import_id'])
+            ));
         }
     }
 }
